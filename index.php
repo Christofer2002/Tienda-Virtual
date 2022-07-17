4fd1e783b114b8b27bcc0bf48257e192ea2f6ad1
Christofer Daniel Chaves Pauth <cristoferchaves36@gmail.com>
2022-07-16 18:55:19 -0600

    Tienda virtual actualizada

--- index.php
+++ index.php
@@ -24,14 +24,14 @@
         <section>
             <nav class="navbar navbar-expand-lg navbar-light bg-light navbar-dark bg-dark">
                 <div class="container-fluid">
-                  <a class="navbar-brand tienda-marca" href="index.html"><h3>Tienda DyD</h3></a>
+                  <a class="navbar-brand tienda-marca" href="index.php"><h3>Tienda DyD</h3></a>
                   <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                   </button>
                   <div class="collapse navbar-collapse" id="navbarScroll">
                     <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
                       <li class="nav-item">
-                        <a class="nav-link container__fluid--activo" aria-current="page" href="../index.html">Inicio</a>
+                        <a class="nav-link container__fluid--activo" aria-current="page" href="../index.php">Inicio</a>
                       </li>
                       <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
@@ -67,6 +67,8 @@
         </footer>
     </main>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-modal/0.9.1/jquery.modal.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
 </body>
 </html>
